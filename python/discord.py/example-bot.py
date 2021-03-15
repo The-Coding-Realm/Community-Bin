2be91278d60b5f372e37bbc3eb8a181bd40f85f2
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-03-15 23:10:39 +0530

    Format files and sort imports

--- python/discord.py/example-bot.py
+++ python/discord.py/example-bot.py
@@ -2,21 +2,24 @@
 import discord
 from discord.ext import commands
 
+bot = commands.Bot(command_prefix=".", description="A basic discord bot")
 
-bot = commands.Bot(command_prefix='.',
-                   description='A basic discord bot')
 
 @bot.event
 async def on_ready():
     print("I'm online!")
 
-@bot.command(name='ping')
+
+@bot.command(name="ping")
 async def _ping(ctx):
     latency = bot.latency * 1000  # convert to ms
-    embed = discord.Embed(title='Pong!',  # make an embed to send
-                          description=f'My latency is {latency:.2f}ms')
+    embed = discord.Embed(
+        title="Pong!",  # make an embed to send
+        description=f"My latency is {latency:.2f}ms",
+    )
 
     await ctx.send(embed=embed)
 
-if __name__ == '__main__':  # make sure the file isn't being imported
-    bot.run('YOUR_TOKEN_HERE')  # put your own bot token in here
+
+if __name__ == "__main__":  # make sure the file isn't being imported
+    bot.run("YOUR_TOKEN_HERE")  # put your own bot token in here
