ef94e5d8f267bf9361f8127d81847c51fdfa5c04
Mossyegghead01 <luthfihaidar1176@gmail.com>
2021-03-26 22:40:15 +0700

    dealling with pylint

--- python/python-module/setup.py
+++ python/python-module/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read() # get readme and import it for long description of module, you may delete this if you want to manually write it
 
 setup(
-    name='MyModule', # Your module name
+    name='MyModule',  # Your module name
     packages=find_packages(include=['Mymodule']), # Your module file
-    version='0.1.0', # module version
-    description='My module made with python!', # module short description
-    long_description=long_description, # module long description, if you want to manually write it change the variable to string and type your long description
-    long_description_content_type="text/markdown", # long description file type (in case of this because its .md we use text/markdown)
-    url="https://github.com/somone/a_repostory", # link to your github, this is optional
-    author='Me', # the author of the module, can be more than one
-    license='MIT', # module lisence
-    install_requires=[], # required packages (if there is one)
+    version='0.1.0',  # module version
+    description='My module made with python!',  # module short description
+    long_description=long_description,  # module long description, if you want to manually write it change the variable to string and type your long description
+    long_description_content_type="text/markdown",  # long description file type (in case of this because its .md we use text/markdown)
+    url="https://github.com/somone/a_repostory",  # link to your github, this is optional
+    author='Me',  # the author of the module, can be more than one
+    license='MIT',  # module lisence
+    install_requires=[],  # required packages (if there is one)
 )
