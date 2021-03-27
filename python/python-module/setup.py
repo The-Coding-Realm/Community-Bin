51206173cd973531774a676ce5f30377f9bf2a74
Mossyegghead01 <luthfihaidar1176@gmail.com>
2021-03-27 08:00:04 +0700

    flake8 again

--- python/python-module/setup.py
+++ python/python-module/setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()  # get readme and import it for long description of module
+    long_description = fh.read()  # get readme and import it
+    # for long description of module
     # you may delete this if you want to manually write it
 
 setup(
@@ -10,10 +11,10 @@ setup(
     version='0.1.0',  # module version
     description='My module made with python!',  # module short description
     long_description=long_description,  # module long description
-    # if you want to manually write it change the variable to string 
+    # if you want to manually write it change the variable to string
     # and type your long description
-    long_description_content_type="text/markdown",  # long description file type 
-    # (in case of this because its .md we use text/markdown)
+    long_description_content_type="text/markdown",  
+    # long description file type (in case of this because its .md we use text/markdown)
     url="https://github.com/somone/a_repostory",  # link to your github
     # this is optional
     author='Me',  # the author of the module, can be more than one
