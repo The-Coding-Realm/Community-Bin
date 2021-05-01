12626e442c9b1984435bcfed767bce44ec776c3b
Makiyu <73825066+Makiyu-py@users.noreply.github.com>
2021-05-01 11:25:42 +0800

    make bot example load the api cog

--- python/discord.py/example-bot.py
+++ python/discord.py/example-bot.py
@@ -24,5 +24,8 @@ async def _ping(ctx):
 
 bot.add_command(_ping)
 
+# you can uncomment the line below if you have the api-cog-example cog already
+#bot.load_extension("api-cog-example")
+
 if __name__ == "__main__":  # make sure the file isn't being imported
     bot.run("YOUR_TOKEN_HERE")  # put your own bot token in here
